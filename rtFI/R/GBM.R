1908dd3d071a2d18524b367291115a7a93645aff
Brayden Boyko <braydenboyko@boykowealth.com>
2025-02-09 14:29:46 -0700

    Added rtFI package

---
+++ rtFI/R/GBM.R
@@ -0,0 +1,29 @@
+#' Geometric Brownian Motion Simulation
+#'
+#' This function simulates a geometric Brownian motion.
+#'
+#' @param spot Initial spot price
+#' @param drift Drift rate
+#' @param sigma Volatility
+#' @param T2M Time to maturity
+#' @param delta_T Time step
+#' @param nsims Number of simulations
+#' @return A tibble with simulated paths
+#' @export
+
+GBM <- function(spot, drift, sigma, T2M, delta_T, nsims){
+  
+  periods <- T2M / delta_T
+  diffusion <- NULL
+  
+  diffusion <- matrix(stats::rnorm(periods * nsims, mean = 0, sd = sqrt(delta_T)), ncol = nsims, nrow = periods)
+  S <- exp((drift - (sigma^2) / 2) * delta_T + sigma * diffusion)
+  S <- apply(rbind(rep(spot,nsims),S), 2, cumprod)
+  S <- dplyr::as_tibble(S, .name_repair = "minimal")
+  names(S) <- paste("sim", 1:nsims, sep = "")
+  S <- S %>% dplyr::mutate(t = seq(0, T2M, delta_T)) %>% dplyr::select(t, dplyr::everything())
+  
+  return(S)
+}
+
+#GBM(spot=10, drift=0, sigma=0.2, T2M=1, delta_T=1/12, nsims=2)
