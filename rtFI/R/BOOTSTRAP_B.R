e64b37546f8175db45f48911ac8d192f053d6d98
Brayden Boyko <braydenboyko@boykowealth.com>
2025-02-09 22:58:20 -0700

    Finalized Bootstrap Bonds

--- rtFI/R/BOOTSTRAP_B.R
+++ rtFI/R/BOOTSTRAP_B.R
@@ -7,46 +7,48 @@
 #' @return A tibble with maturities and zero rates.
 #' @export
 #' @examples
-#' bond_data <- tibble(maturity = c(1, 2, 3, 4),
-#'                     price = c(98, 97, 95, 92),
-#'                     coupon = c(0.05, 0.05, 0.05, 0.05))
-#' BOOTSTRAP_B(data = bond_data)
+#'bonds <- data.frame(
+#'           Maturity = c(1, 2, 3),
+#'           Price = c(950, 920, 880),
+#'           Coupon = c(0, 50, 60),
+#'           FaceValue = 1000
+#'           )
+#' BOOTSTRAP_B(bond_data = bond_data)
 
-BOOTSTRAP_B <- function(data) {
+BOOTSTRAP_B <- function(bond_data) {
   
-  n <- nrow(data)
-  zero_rates <- numeric(n)
+  zero_rates <- numeric(length = nrow(bond_data))
   
-  for (i in 1:n) {
-    maturity <- data$maturity[i]
-    price <- data$price[i]
-    coupon <- data$coupon[i]
-    
-    if (i == 1) {
-      discount_factors[i] <- 1 / (1 + coupon)
-    } else {
-      sum_df <- 0
-      for (j in 1:(i-1)) {
-        sum_df <- sum_df + coupon * discount_factors[j]
-      }
-      # The discount factor for the current bond
-      discount_factors[i] <- (coupon * sum_df + 1) / price
-    }
-    
-    # Calculate the zero rates from the discount factors
-    zero_rates[i] <- sqrt(1 / discount_factors[i]) - 1
+  calculate_first_zero_rate <- function(price, face_value) {
+    r <- (face_value / price) - 1
+    return(r)
   }
   
-  result <- dplyr::tibble(
-    maturity = data$maturity,
-    zero_rate = zero_rates
-  )
+  calculate_zero_rate <- function(bond, prev_rates) {
+    price <- bond$Price
+    coupon <- bond$Coupon
+    face_value <- bond$FaceValue
+    maturity <- bond$Maturity
+    
+    pv_coupons <- sum(purrr::map_dbl(1:(maturity - 1), function(t) {
+      coupon / (1 + prev_rates[t])^t
+    }))
+    
+    r <- ((face_value + coupon - pv_coupons) / price)^(1 / maturity) - 1
+    return(r)
+  }
   
-  return(result)
+  for (i in 1:nrow(bond_data)) {
+    if (i == 1) {
+      zero_rates[i] <- calculate_first_zero_rate(bond_data$Price[i], bond_data$FaceValue[i])
+    } else {
+      zero_rates[i] <- calculate_zero_rate(bond_data[i, ], zero_rates)
+    }
+  }
+  
+  bond_data$ZeroRate <- zero_rates
+  
+  return(bond_data %>% dplyr::select(Maturity, ZeroRate))
 }
 
-bond_data <- tibble(maturity = c(1, 2, 3, 4),
-                    price = c(98, 97, 95, 92),
-                    coupon = c(0.05, 0.05, 0.05, 0.05))
-BOOTSTRAP_B(data = bond_data)
 
